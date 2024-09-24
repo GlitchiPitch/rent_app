ec3ed45d09bd89896be3026fdd3a0370d28c0429
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-24 08:37:00 +0500

    Create endpoints for api v1, create schemas for ads

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,10 +1,28 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from core.schemas import AdAdd, AdShow
 
 paths = settings.api.v1
 
 router = APIRouter(
     prefix=paths.prefix
 )
+
+async def _get_all_ads():
+    ...
+
+#TODO how to show return for json
+async def _new_ads(body: AdAdd):
+    return {
+        'status': 200,
+        'message': AdShow(**body.model_dump())
+    }
+@router.get(paths.get_all_ads)
+async def get_all_ads():
+    ...
+
+@router.post(paths.new_ads)
+async def new_ads():
+    ...
 
